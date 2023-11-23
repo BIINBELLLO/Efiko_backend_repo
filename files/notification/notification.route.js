b3b47e6bccbf1ad358bf28d6553b8b05fcb98f02
codewithemmy <codewithemmy@gmail.com>
2023-11-23 22:09:32 +0100

    update

--- files/notification/notification.route.js
+++ files/notification/notification.route.js
@@ -1,11 +1,9 @@
 const notificationRoute = require("express").Router()
 const { isAuthenticated } = require("../../utils")
 
-notificationRoute.use(isAuthenticated)
-
 const { fetchNotifications } = require("./notification.controller")
-
 notificationRoute.use(isAuthenticated)
+
 //route
 notificationRoute.route("/").get(fetchNotifications)
 

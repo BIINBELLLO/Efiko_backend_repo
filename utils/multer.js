66ec5d6ee6713a4564ab1877d3ce1120464b25c0
codewithemmy <codewithemmy@gmail.com>
2024-01-09 11:47:38 +0100

    new changes

--- utils/multer.js
+++ utils/multer.js
@@ -14,7 +14,7 @@ const uploadManager = (destination) => {
     storage: new CloudinaryStorage({
       cloudinary: cloudinary,
       params: {
-        folder: `ZillightLab/WhoUEpp/${destination}`,
+        folder: `efiko/${destination}`,
       },
     }),
   })

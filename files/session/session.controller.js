c14fd099c50e29fdb7a5160d382b0c4d4875a20a
codewithemmy <codewithemmy@gmail.com>
2024-02-04 16:38:35 +0100

    new changes

--- files/session/session.controller.js
+++ files/session/session.controller.js
@@ -3,6 +3,7 @@ const { responseHandler } = require("../../core/response")
 const { manageAsyncOps } = require("../../utils")
 const { CustomError } = require("../../utils/errors")
 const { SessionService } = require("./session.service")
+const crypto = require("crypto")
 
 const createSessionController = async (req, res, next) => {
   const [error, data] = await manageAsyncOps(
@@ -80,16 +81,73 @@ const getZoomSessionController = async (req, res, next) => {
 }
 
 const zoomWebhookController = async (req, res, next) => {
-  console.log("req", req)
-  console.log("req.body", req.body)
-  const [error, data] = await manageAsyncOps(
-    SessionService.zoomSessionWebhookService(req.body)
-  )
-  if (error) return next(error)
+  // const [error, data] = await manageAsyncOps(
+  //   SessionService.zoomSessionWebhookService(req.body)
+  // )
+  // )
+  let response
 
-  if (!data.success) return next(new CustomError(data.msg, BAD_REQUEST, data))
+  console.log(req.headers)
+  console.log(req.body)
 
-  res.status(200).end()
+  // construct the message string
+  const message = `v0:${req.headers["x-zm-request-timestamp"]}:${JSON.stringify(
+    req.body
+  )}`
+
+  const hashForVerify = crypto
+    .createHmac("sha256", process.env.ZOOM_WEBHOOK_SECRET_TOKEN)
+    .update(message)
+    .digest("hex")
+
+  // hash the message string with your Webhook Secret Token and prepend the version semantic
+  const signature = `v0=${hashForVerify}`
+
+  // you validating the request came from Zoom
+  if (req.headers["x-zm-signature"] === signature) {
+    // Zoom validating you control the webhook endpoint
+    if (req.body.event === "endpoint.url_validation") {
+      const hashForValidate = crypto
+        .createHmac("sha256", process.env.ZOOM_WEBHOOK_SECRET_TOKEN)
+        .update(req.body.payload.plainToken)
+        .digest("hex")
+
+      response = {
+        message: {
+          plainToken: req.body.payload.plainToken,
+          encryptedToken: hashForValidate,
+        },
+        status: 200,
+      }
+
+      console.log(response.message)
+
+      res.status(response.status)
+      res.json(response.message)
+    } else {
+      response = {
+        message: "Authorized request to Zoom Webhook sample.",
+        status: 200,
+      }
+
+      console.log(response.message)
+
+      res.status(response.status)
+      res.json(response)
+
+      // business logic here, example make API request to Zoom or 3rd party
+    }
+  } else {
+    response = {
+      message: "Unauthorized request to Zoom Webhook sample.",
+      status: 401,
+    }
+
+    console.log(response.message)
+
+    res.status(response.status)
+    res.json(response)
+  }
 }
 
 module.exports = {

158aa6c3496e86ddc705865af8588ca4d88f1bce
codewithemmy <codewithemmy@gmail.com>
2024-02-29 08:07:07 +0100

    mmm

--- files/session/session.service.js
+++ files/session/session.service.js
@@ -341,16 +341,18 @@ class SessionService {
 
     const currentDatePlus24Hours = new Date()
     currentDatePlus24Hours.setHours(currentDatePlus24Hours.getHours() + 24)
+    let recorded = { type: "not-recorded" }
     let extras = {}
     // if (locals.accountType === "student" || locals.accountType === "tutor") {
     extras = { date: { $gte: currentDatePlus24Hours } }
     // }
-    // if (params.type && params.type === "recorded") {
-    //   recorded = {}
-    // }
-    // console.log("error", recorded)
+    if (params.type && params.type === "recorded") {
+      recorded = {}
+    }
+
     const sessions = await SessionRepository.findAllSessionParams({
       ...params,
+      ...recorded,
       limit,
       skip,
       sort,
